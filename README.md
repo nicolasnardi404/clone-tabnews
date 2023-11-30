d2254542cc611ed8710f7830504c9637a6353266
nicolasnardi404 <151576395+nicolasnardi404@users.noreply.github.com>
2023-11-30 18:53:05 +0000

    adiciona scripts  e

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto de clone do tabnews feito pro curso do Filipe Deschamps curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1> TESTE do TESTE nova versao</h1>
+function Home() {
+  return <h1> TESTE do TESTE nova versao</h1>;
 }
 
 export default Home;
